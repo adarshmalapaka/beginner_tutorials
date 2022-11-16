570bf7583785ee8d767a2931078256bf63b61cb8
Adarsh Malapaka <adarshmalapaka98@gmail.com>
2022-11-16 02:22:35 -0500

    Invoke RCLCPP WARN for no publisher on the subscribed topic

--- src/subscriber_member_function.cpp
+++ src/subscriber_member_function.cpp
@@ -13,17 +13,24 @@
 // limitations under the License.
 
 #include "../include/beginner_tutorials/MinimalSubscriber.hpp"
+#include <string>
 
 MinimalSubscriber::MinimalSubscriber()
   : Node("minimal_subscriber") {
      subscription_ = this->create_subscription<std_msgs::msg::String>(
        "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+
+     if (this->count_publishers("topic") == 0) {
+        RCLCPP_WARN(this->get_logger(),
+              "No publisher found on the subscribed topic!");
+     }
 }
 
 void MinimalSubscriber::topic_callback(
   const std_msgs::msg::String::SharedPtr msg) const {
-  RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+    RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
 }
+
 
 int main(int argc, char * argv[]) {
   rclcpp::init(argc, argv);
